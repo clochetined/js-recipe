e9fe8282241a98b630bbe6b5619398c989cfdb55
clochetined <naishutiantou@gmail.com>
2023-07-16 15:22:53 +0900

    stopwatch

--- 02_レシピ/01_ストップウォッチ/main.js
+++ 02_レシピ/01_ストップウォッチ/main.js
@@ -6,20 +6,21 @@ let count = 0
 let id = null
 
 const countUp = function () {
-  // count を更新
-  count += 1
-  // count を秒単位にして表示
-  display.textContent = (count / 100).toFixed(2)
+  count += 0.01
+  display.textContent = count
+  console.log(count)
 }
 
 startButton.addEventListener("click", function () {
   if (id === null) {
     id = setInterval(countUp, 10)
     startButton.textContent = "Stop"
+    console.log(count)
   } else {
     clearInterval(id)
     id = null
     startButton.textContent = "Start"
+    console.log(count)
   }
 })
 
