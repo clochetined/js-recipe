a47cd7e97d87f6ecbc39537490074b6d47b09fd6
clochetined <naishutiantou@gmail.com>
2023-07-25 23:13:48 +0900

    関数の分離

--- 04_ゼミ/week2/session/main.js
+++ 04_ゼミ/week2/session/main.js
@@ -2,6 +2,47 @@ const InputText = document.getElementById("memo-input")
 const AddButton = document.getElementById("add-button")
 const cardContainer = document.getElementById("memo-container")
 
+AddButton.addEventListener("click", () => {
+  createElement("div", "button", "delete", cardContainer)
+  clearInputtext()
+})
+
+const createElement = (tag1, tag2, text, parent) => {
+  const card = document.createElement(tag1)
+  const deleteButton = document.createElement(tag2)
+
+  card.textContent = InputText.value
+  deleteButton.textContent = text
+  parent.appendChild(card)
+  card.appendChild(deleteButton)
+
+  deleteButton.addEventListener("click", () => {
+    deleteCard(card)
+  })
+}
+
+const clearInputtext = () => {
+  InputText.value = ""
+}
+
+const deleteCard = (card) => {
+  card.remove()
+}
+
+// const addfunction = () => {
+//   const card = this.document.createElement("div")
+//   const deleteButton = this.document.createElement("button")
+
+//   card.textContent = InputText.value
+//   deleteButton.textContent = "delete"
+//   cardContainer.appendChild(card)
+//   card.appendChild(deleteButton)
+// InputText.value = ""
+//   deleteButton.addEventListener("click", () => {
+//     card.remove()
+//   })
+// }
+
 // AddButton.addEventListener("click", () => {
 //   const card = document.createElement("div")
 //   const deleteButton = document.createElement("button")
@@ -17,22 +58,3 @@ const cardContainer = document.getElementById("memo-container")
 // })
 
 // ここから下は関数を使ったもの
-
-AddButton.addEventListener("click", () => {
-  addfunction()
-})
-
-const addfunction = () => {
-  const card = this.document.createElement("div")
-  const deleteButton = this.document.createElement("button")
-
-  card.textContent = InputText.value
-  deleteButton.textContent = "delete"
-  cardContainer.appendChild(card)
-  card.appendChild(deleteButton)
-  InputText.value = ""
-
-  deleteButton.addEventListener("click", () => {
-    card.remove()
-  })
-}
