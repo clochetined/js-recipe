bb3b8c8786a7cf8aaf88b24e2ab12a6a4730a5b5
clochetined <naishutiantou@gmail.com>
2023-08-04 22:49:09 +0900

    複数問題

--- 02_レシピ/02_クイズ（複数）/main.js
+++ 02_レシピ/02_クイズ（複数）/main.js
@@ -4,36 +4,88 @@ const container = document.getElementById("quiz-container")
 const feedback = document.getElementById("feedback")
 
 // クイズの内容
-const quiz = {
-  text: "この星の名前は何でしょう？",
-  image: "Ganymede.jpg",
-  choices: [
-    {
-      text: "ゴリアテ",
-      feedback:
-        "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
-    },
-    {
-      text: "ゼニガメ",
-      feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
-    },
-    {
-      text: "ガニメデ",
-      feedback: "正解！ガニメデは、木星の第三惑星だよ！",
-    },
-    {
-      text: "メガロテ",
-      feedback: "残念！メガロテは、ガンダムの敵だよ。",
-    },
-    {
-      text: "ガンダム",
-      feedback: "残念！ガンダムは、ガンダムの主人公だよ。",
-    },
-  ],
-}
+const quizzes = [
+  {
+    text: "この星の名前は何でしょう？",
+    image: "Ganymede.jpg",
+    choices: [
+      {
+        text: "ゴリアテ",
+        isCorrect: false,
+        feedback:
+          "残念！ゴリアテは、旧約聖書に登場するダビデに石で殺される巨人だよ。",
+      },
+      {
+        text: "ゼニガメ",
+        isCorrect: false,
+        feedback: "残念！ゼニガメは、クサガメまたはニホンイシガメの幼体だよ。",
+      },
+      {
+        text: "ガニメデ",
+        isCorrect: true,
+        feedback: "正解！ガニメデは、木星の第三惑星だよ！",
+      },
+      {
+        text: "メガロテ",
+        isCorrect: false,
+        feedback: "残念！メガロテは、ガンダムの敵だよ。",
+      },
+      {
+        text: "ガンダム",
+        isCorrect: false,
+        feedback: "残念！ガンダムは、ガンダムの主人公だよ。",
+      },
+    ],
+  },
+  {
+    text: "いま、何問目？",
+    image: "Two.jpeg",
+    choices: [
+      {
+        text: "１",
+        isCorrect: false,
+        feedback: "残念！ひとつ少ないよ。",
+      },
+      {
+        text: "２",
+        isCorrect: true,
+        feedback: "正解！１でも３でもないよ！",
+      },
+      {
+        text: "３",
+        isCorrect: false,
+        feedback: "残念！ひとつ多いよ。",
+      },
+    ],
+  },
+  {
+    text: "この城の名前は？",
+    image: "Maruoka.png",
+    choices: [
+      {
+        text: "丸岡城",
+        isCorrect: true,
+        feedback: "正解！どこからどうみても丸岡城だね。",
+      },
+      {
+        text: "丸亀城",
+        isCorrect: false,
+        feedback: "残念！どこからどうみても丸亀城ではないよ。",
+      },
+      {
+        text: "丸子城",
+        isCorrect: false,
+        feedback: "残念！どこからどうみても丸子城ではないよ。",
+      },
+    ],
+  },
+]
 
 // quiz を画面に表示する関数
-const reloadQuiz = function () {
+const reloadQuiz = function (quizNumber) {
+  // quizNumber番目のクイズを取得
+  const quiz = quizzes[quizNumber]
+
   // 問題文を表示
   quizText.textContent = "Q. " + quiz.text
 
@@ -51,16 +103,21 @@ const reloadQuiz = function () {
     // ボタンをクリックしたら正誤判定
     button.onclick = function () {
       // 正解かどうかチェック
-      choose(i)
+      choose(quizNumber, i)
+      if (quiz.choices[i].isCorrect === true) {
+        alert("正解です！")
+        const nextQuizButton = document.createElement("button")
+        nextQuizButton.textContent = "次の問題に挑戦する"
+        feedback.appendChild(nextQuizButton)
+      }
     }
   }
 }
-
 // choiceNumber番目の選択肢を選択
-const choose = function (choiceNumber) {
+const choose = function (quizNumber, choiceNumber) {
   // フィードバックを表示
-  feedback.textContent = quiz.choices[choiceNumber].feedback
+  feedback.textContent = quizzes[quizNumber].choices[choiceNumber].feedback
 }
 
 // reloadQuiz関数 を実行して、クイズを画面に表示する
-reloadQuiz()
+reloadQuiz(0)
